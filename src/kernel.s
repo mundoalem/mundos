4d44ee55b50075bd1042f7397d6032060a95a6f4
Egon Braun <egon@mundoalem.io>
2024-03-17 18:03:54 +0000

    Updates src/boot.s and src/kernel.s

--- src/kernel.s
+++ src/kernel.s
@@ -1,9 +1,24 @@
+/*
+This file is part of mundOS.
+
+The mundOS is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+The mundOS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+details.
+
+You should have received a copy of the GNU Affero General Public License along with mundOS. If not, see
+<https://www.gnu.org/licenses/>.
+*/
+
 .global _kernel
 
 .data
 
 uartdr:     .quad   0x9000000
-banner:     .asciz  "mundOS v1.0.0\n"
+banner:     .asciz  "mundOS v0.0.1\n"
 
 .text
 
